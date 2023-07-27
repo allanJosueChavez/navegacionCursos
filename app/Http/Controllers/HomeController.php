c4bc3cafaff48091025e0042b04b401627cc816c
Martin <3014683970101@ingenieria.usac.edu.gt>
2023-07-26 19:45:55 -0600

    Push to master

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+/*class HomeController extends Controller
 {
 //asingnar un controllador a una rotas
     public function __invoke(){
         return "Bienvenido a mi pagina principal";
     }
 }
+*/
